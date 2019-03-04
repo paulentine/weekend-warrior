0507988a1bfbf8c0f736e3ff5589f60dea485966
Pauline Sauget <paulinevsauget@gmail.com>
2019-03-03 16:43:52 -0800

    Created class: Robot with methods: initialize & reset

--- 02-robot-name/robot_name.rb
+++ 02-robot-name/robot_name.rb
@@ -1,1 +1,12 @@
 
+class Robot
+  attr_reader :name
+  
+  def initialize
+    @name = rand(65..90).chr + rand(65..90).chr + rand(100..999).to_s
+  end
+
+  def reset
+    @name = rand(65..90).chr + rand(65..90).chr + rand(100..999).to_s
+  end
+end
