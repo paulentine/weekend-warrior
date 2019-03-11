b985064314f53b750fea94c4b567e71f19f63ab8
Pauline Sauget <paulinevsauget@gmail.com>
2019-03-10 23:44:50 -0700

    Wrote Hamming.compute

---
+++ 03-hamming/hamming.rb
@@ -0,0 +1,11 @@
+class Hamming
+  def self.compute(strand_one, strand_two)
+    distance, i = 0, 0
+    raise ArgumentError if strand_one.length != strand_two.length
+    return 0 if strand_one == strand_two
+    strand_one.length.times do |i|
+      distance += 1 if strand_one[i] != strand_two[i]
+    end
+    return distance
+  end
+end
