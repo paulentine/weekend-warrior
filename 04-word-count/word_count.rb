126953f6bdb9f5cba092312ef73bc59f82eb7956
Pauline Sauget <paulinevsauget@gmail.com>
2019-03-17 14:02:01 -0700

    Added method words to count words in a string

--- 04-word-count/word_count.rb
+++ 04-word-count/word_count.rb
@@ -1,1 +1,9 @@
-
+def words(string)
+  # Default value for key: 0
+  words_with_count = Hash.new(0)
+  words = string.split(" ")
+  words.each do |word|
+    words_with_count[word] += 1
+  end
+  return words_with_count
+end
