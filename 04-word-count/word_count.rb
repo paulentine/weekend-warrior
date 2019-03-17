d80266ab7e57d57efdb85c26d567ddb408fb9afe
Pauline Sauget <paulinevsauget@gmail.com>
2019-03-17 14:04:46 -0700

    Refactor: eliminate unnecessary var

--- 04-word-count/word_count.rb
+++ 04-word-count/word_count.rb
@@ -1,8 +1,7 @@
 def words(string)
-  # Default value for key: 0
+  # Default value for keys: 0
   words_with_count = Hash.new(0)
-  words = string.split(" ")
-  words.each do |word|
+  string.split(" ").each do |word|
     words_with_count[word] += 1
   end
   return words_with_count
